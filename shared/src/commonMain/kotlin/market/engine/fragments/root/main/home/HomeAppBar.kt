5b58e32ac2fb2514edee10fa19ba58bf32aea57d
WladyslawPopov <168862200+WladyslawPopov@users.noreply.github.com>
2025-03-05 19:27:19 +0100

    commit "3" Initial setup for Kotlin Multiplatform project targeting Android, iOS, and Desktop
    - Created `README.md` with project description and folder structure.
    - Added `.gitignore` to exclude build artifacts and other unnecessary files.
    - Added `AppIcon` and `AccentColor` to `Assets.xcassets`.
    - Created `Config.xcconfig` for configuration settings like `TEAM_ID`, `BUNDLE_ID`, and `APP_NAME`.
    - Created `Preview Assets.xcassets` for preview assets.
    - Created basic `Info.plist` for iOS app.
    - Created `iOSApp.swift` as the entry point for the iOS app.
    - Added `ComposeView` and `ContentView` in `ContentView.swift`.
    - Updated `project.pbxproj` to set up the iOS project structure, including build phases, targets, configurations, and file references.
    - Created new files for `Preview Content`.
    - Updated gradle wrapper version to 8.9.
    - Added app icon to `AppIcon.appiconset`.

--- shared/src/commonMain/kotlin/market/engine/fragments/root/main/home/HomeAppBar.kt
+++ shared/src/commonMain/kotlin/market/engine/fragments/root/main/home/HomeAppBar.kt
@@ -1,17 +1,19 @@
 package market.engine.fragments.root.main.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import market.engine.core.data.globalData.ThemeResources.colors
 import market.engine.core.data.globalData.ThemeResources.dimens
 import market.engine.core.data.globalData.ThemeResources.drawables
@@ -69,11 +71,10 @@ fun HomeAppBar(
         modifier = modifier
             .fillMaxWidth().wrapContentHeight(),
         title = {
-            Icon(
+            Image(
                 painter = painterResource(drawables.logo),
                 contentDescription = stringResource(strings.homeTitle),
-                modifier = modifier,
-                tint = colors.titleTextColor,
+                modifier = Modifier.size(140.dp, 68.dp),
             )
         },
         navigationIcon = {
