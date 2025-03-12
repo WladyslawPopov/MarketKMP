81a3750cbff108b3c9b1db8b62a494cb1e3ebf85
WladyslawPopov <168862200+WladyslawPopov@users.noreply.github.com>
2025-03-13 00:22:11 +0100

    commit "1.48" Integrate Yandex Login SDK and update app settings
    - Added Yandex Login SDK support for iOS.
    - Created `YandexLoginButton.swift` for Yandex login UI.
    - Updated `Info.plist`: added Yandex URL schemes and domain, INIntents.
    - Created `LoginAppLinks.plist` for associated domains.
    - Updated `AppDelegate`: added Yandex SDK initialization, handling URLs, and user activities.
    - Modified `additionalAuthorizationContent` to include Yandex login button.
    - Fixed a typo in `strings.xml`: "сатвка" to "ставка".
    - Updated `RootView` to reflect changes in view controllers.
    - Added a `Column` composable to display additional authentication options in a vertical layout, using the `smallPadding` dimension for spacing.

--- shared/src/iosMain/kotlin/market/engine/common/getAddittionalAutorizationContent.ios.kt
+++ shared/src/iosMain/kotlin/market/engine/common/getAddittionalAutorizationContent.ios.kt
@@ -1,22 +1,26 @@
 package market.engine.common
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.UIKitView
 import market.engine.core.data.globalData.AuthManager
 import market.engine.core.data.globalData.ThemeResources.colors
+import market.engine.core.data.globalData.ThemeResources.dimens
 import platform.UIKit.UIView
 
-var additionalAuthContent: UIView? = null
+var additionalAuthContent: List<UIView?> = emptyList()
 
 @Composable
 actual fun additionalAuthorizationContent(onSuccess: (HashMap<String, String>) -> Unit) {
@@ -31,18 +35,26 @@ actual fun additionalAuthorizationContent(onSuccess: (HashMap<String, String>) -
         }
     }
 
-    if (additionalAuthContent != null) {
-        Box(
-            modifier = Modifier.background(colors.transparent).fillMaxSize()
+    Box(
+        modifier = Modifier.background(colors.transparent).fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(dimens.smallPadding)
         ) {
-            UIKitView(
-                factory = { additionalAuthContent!! },
-                modifier = Modifier
-                    .background(colors.transparent)
-                    .height(44.dp)
-                    .fillMaxWidth()
-                    .clip(MaterialTheme.shapes.medium)
-            )
+            additionalAuthContent.forEach { content ->
+                if (content != null) {
+                    UIKitView(
+                        factory = { content },
+                        modifier = Modifier
+                            .background(colors.transparent)
+                            .height(44.dp)
+                            .fillMaxWidth(0.7f)
+                            .clip(MaterialTheme.shapes.medium)
+                    )
+                }
+            }
         }
     }
 }
