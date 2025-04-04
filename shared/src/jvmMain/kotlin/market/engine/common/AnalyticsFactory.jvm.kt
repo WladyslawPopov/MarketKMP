469f9fe2cff7f90f828e835fa27fb38a59ac12dc
WladyslawPopov <168862200+WladyslawPopov@users.noreply.github.com>
2025-04-04 15:35:24 +0200

    ``` commit Update to 5.0.55, add desktop analytics, and enhance error handling
    - Updated the app version to 5.0.55 in `build.gradle.kts`.
    - Added a `DesktopAnalyticsHelper` to `desktopMain` to enable analytics reporting on desktop.
    - Implemented `AnalyticsFactory` to provide an analytics helper instance, using the desktop implementation on JVM platforms.
    - Changed `FirebaseMessagingService` to stop printing the token device.
    - Modified `main.kt` to use `DesktopAnalyticsHelper`.
    - Updated `AnalyticsFactory.jvm.kt` to use the `DesktopAnalyticsHelper` or a default logger.
    - Added try-catch blocks for `FileKit.init`, Firebase Messaging, YandexAuth, Firebase remote config and others in `MainActivity.kt` to improve error handling.
    - Added  `VkAuthorization` and `YandexAuthorization` in try-catch blocks.
    - Implemented integrity-token to try-catch.
    - Added try-catch logic for the update of the app.
    - Added logic for `showReviewManager` in try-catch.
    - Added try-catch for the initialization of the AppMetrica and VK in `AuctionApp`.
    - Modified `YandexAnalyticsHelper` to use try-catch in `setUserProfileID`.
    - Added `reportEvent`, `updateUserProfile`, `setUserProfileID` functions in `DesktopAnalyticsHelper`.
    - Modified `Info.plist` to update CFBundleShortVersionString.
    - Update UserInterfaceState.xcuserstate.
    ```

--- shared/src/jvmMain/kotlin/market/engine/common/AnalyticsFactory.jvm.kt
+++ shared/src/jvmMain/kotlin/market/engine/common/AnalyticsFactory.jvm.kt
@@ -2,9 +2,26 @@ package market.engine.common
 
 import market.engine.core.analytics.AnalyticsHelper
 
+var analyticsHelper: AnalyticsHelper? = null
 
 actual object AnalyticsFactory {
     actual fun getAnalyticsHelper(): AnalyticsHelper {
-        return DesktopAnalyticsHelper
+        return if (analyticsHelper == null) {
+            object : AnalyticsHelper {
+                override fun reportEvent(eventName: String, eventParameters: Map<String, Any?>) {
+                    println("Event: $eventName, Parameters: $eventParameters")
+                }
+
+                override fun updateUserProfile(attributes: Map<String, Any?>) {
+                    println("User Profile Updated: $attributes")
+                }
+
+                override fun setUserProfileID(userID: String) {
+                    println("User ID Set: $userID")
+                }
+            }
+        }else{
+            analyticsHelper!!
+        }
     }
 }
